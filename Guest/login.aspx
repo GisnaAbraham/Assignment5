6040473b66495d8d83b168edea368237d0244540
GisnaAbraham <114311093+GisnaAbraham@users.noreply.github.com>
2022-12-03 22:45:06 +0530

    Add files via upload

---
+++ Guest/login.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Guest/Guest.Master" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="assignment5.Guest.login" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <h1><center>LOGIN FORM</center></h1>
+                       
+              
+     <table align="center" bgcolor="white" >
+                
+     
+                <tr>
+                    <td>Username</td>
+                    <td>
+                        <asp:TextBox ID="uname" runat="server" required></asp:TextBox><br /><br />
+                    </td>
+                </tr>
+                <tr>
+                    <td> Password</td>
+                    <td>
+                        <asp:TextBox ID="pswd" runat="server" TextMode="Password" Required></asp:TextBox><br /><br />
+                    </td>
+                </tr>
+                
+        <tr>
+            <td>
+                <asp:Button ID="Button1" runat="server" Text="Register" OnClick="Button1_Click" />
+            </td>
+        </tr>
+                         
+            </table>
+</asp:Content>
---
+++ Guest/login.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace assignment5.Guest
+{
+    public partial class login : System.Web.UI.Page
+    {
+        Db db = new Db();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            string sql = "SELECT * FROM login WHERE username='" + uname.Text + "' AND password='" + pswd.Text + "'";
+            DataTable dtobj = db.Executedatatable(sql);
+
+            if (dtobj.Rows.Count == 1)
+            {
+
+                Session["uid"] = dtobj.Rows[0]["login_id"];
+                Session["uname"] = dtobj.Rows[0]["username"].ToString();
+                string a = dtobj.Rows[0]["role"].ToString().Trim();
+
+                if (a == "admin")
+                {
+                    Response.Write("<script>alert('Admin Login ')</script>");
+                    Response.Redirect("../admin/confirm.aspx");
+                }
+                else
+                {
+                    string c = dtobj.Rows[0]["status"].ToString().Trim();
+                    if (c == "Confirm")
+                    {
+                        Response.Write("<script>alert('Successfully logged in')</script>");
+                        Response.Redirect("../employee/details.aspx");
+                    }
+                    else
+                    {
+                        Response.Write("<script>alert('Failed to login')</script>");
+                        Response.Redirect("../Guest/login.aspx");
+                    }
+                }
+            }
+        }
+
+        protected void pswd_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
